211b2ac828386ce27b2eef3cc383152f407f1439
10KJVN <Jahva_game11@hotmail.com>
2023-07-10 18:01:49 +0200

    Services, removed placeholders, Inhoud modal

--- modal.js
+++ modal.js
@@ -141,3 +141,314 @@ window.onclick = function(event) {
     modal5.style.display = "none";
   }
 }
+
+
+
+// Gap for clarity
+
+// Services now
+
+
+var servicemodal1 = document.getElementById("servicemodal1");
+
+// Get the button that opens the modal
+var servicebtn1 = document.getElementById("servicebtn1");
+
+// Get the <span> element that closes the modal
+var servicespan1 = document.getElementsByClassName("serviceclose")[0];
+
+
+servicebtn1.onclick = function() {
+  servicemodal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan1.onclick = function() {
+  servicemodal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodal1) {
+    servicemodal1.style.display = "none";
+  }
+}
+
+
+var servicemodal2 = document.getElementById("servicemodal2");
+
+// Get the button that opens the modal
+var servicebtn2 = document.getElementById("servicebtn2");
+
+// Get the <span> element that closes the modal
+var servicespan2 = document.getElementsByClassName("serviceclose2")[0];
+
+
+servicebtn2.onclick = function() {
+  servicemodal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan2.onclick = function() {
+  servicemodal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    servicemodal2.style.display = "none";
+  }
+}
+
+var servicemodal3 = document.getElementById("servicemodal3");
+
+// Get the button that opens the modal
+var servicebtn3 = document.getElementById("servicebtn3");
+
+// Get the <span> element that closes the modal
+var servicespan3 = document.getElementsByClassName("serviceclose3")[0];
+
+
+servicebtn3.onclick = function() {
+  servicemodal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan3.onclick = function() {
+  servicemodal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodalmodal2) {
+    servicemodal3.style.display = "none";
+  }
+}
+
+
+var servicemodal4 = document.getElementById("servicemodal4");
+
+// Get the button that opens the modal
+var servicebtn4 = document.getElementById("servicebtn4");
+
+// Get the <span> element that closes the modal
+var servicespan4 = document.getElementsByClassName("serviceclose4")[0];
+
+
+servicebtn4.onclick = function() {
+  servicemodal4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan4.onclick = function() {
+  servicemodal4.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodalmodal4) {
+    servicemodal4.style.display = "none";
+  }
+}
+
+var servicemodal5 = document.getElementById("servicemodal5");
+
+// Get the button that opens the modal
+var servicebtn5 = document.getElementById("servicebtn5");
+
+// Get the <span> element that closes the modal
+var servicespan5 = document.getElementsByClassName("serviceclose5")[0];
+
+
+servicebtn5.onclick = function() {
+  servicemodal5.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan5.onclick = function() {
+  servicemodal5.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodalmodal5) {
+    servicemodal5.style.display = "none";
+  }
+}
+
+
+var servicemodal6 = document.getElementById("servicemodal6");
+
+// Get the button that opens the modal
+var servicebtn6 = document.getElementById("servicebtn6");
+
+// Get the <span> element that closes the modal
+var servicespan6 = document.getElementsByClassName("serviceclose6")[0];
+
+
+servicebtn6.onclick = function() {
+  servicemodal6.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan6.onclick = function() {
+  servicemodal6.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodalmodal6) {
+    servicemodal1.style.display = "none";
+  }
+}
+var servicemodal7 = document.getElementById("servicemodal7");
+
+// Get the button that opens the modal
+var servicebtn7 = document.getElementById("servicebtn7");
+
+// Get the <span> element that closes the modal
+var servicespan7 = document.getElementsByClassName("serviceclose7")[0];
+
+
+servicebtn7.onclick = function() {
+  servicemodal7.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan7.onclick = function() {
+  servicemodal7.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodalmodal7) {
+    servicemodal7.style.display = "none";
+  }
+}
+
+
+var servicemodal8 = document.getElementById("servicemodal8");
+
+// Get the button that opens the modal
+var servicebtn8 = document.getElementById("servicebtn8");
+
+// Get the <span> element that closes the modal
+var servicespan8 = document.getElementsByClassName("serviceclose8")[0];
+
+
+servicebtn8.onclick = function() {
+  servicemodal8.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan8.onclick = function() {
+  servicemodal8.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodalmodal8) {
+    servicemodal8.style.display = "none";
+  }
+}
+var servicemodal9 = document.getElementById("servicemodal9");
+
+// Get the button that opens the modal
+var servicebtn9 = document.getElementById("servicebtn9");
+
+// Get the <span> element that closes the modal
+var servicespan9 = document.getElementsByClassName("serviceclose9")[0];
+
+
+servicebtn9.onclick = function() {
+  servicemodal9.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan9.onclick = function() {
+  servicemodal9.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == servicemodalmodal9) {
+    servicemodal9.style.display = "none";
+  }
+}
+
+
+var servicemodal1 = document.getElementById("servicemodal1");
+
+// Get the button that opens the modal
+var servicebtn1 = document.getElementById("servicebtn1");
+
+// Get the <span> element that closes the modal
+var servicespan1 = document.getElementsByClassName("serviceclose")[0];
+
+
+servicebtn1.onclick = function() {
+  servicemodal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan1.onclick = function() {
+  servicemodal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    servicemodal1.style.display = "none";
+  }
+}
+var servicemodal1 = document.getElementById("servicemodal1");
+
+// Get the button that opens the modal
+var servicebtn1 = document.getElementById("servicebtn1");
+
+// Get the <span> element that closes the modal
+var servicespan1 = document.getElementsByClassName("serviceclose")[0];
+
+
+servicebtn1.onclick = function() {
+  servicemodal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan1.onclick = function() {
+  servicemodal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    servicemodal1.style.display = "none";
+  }
+}
+
+
+var servicemodal1 = document.getElementById("servicemodal1");
+
+// Get the button that opens the modal
+var servicebtn1 = document.getElementById("servicebtn1");
+
+// Get the <span> element that closes the modal
+var servicespan1 = document.getElementsByClassName("serviceclose")[0];
+
+
+servicebtn1.onclick = function() {
+  servicemodal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+servicespan1.onclick = function() {
+  servicemodal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    servicemodal1.style.display = "none";
+  }
+}
+
